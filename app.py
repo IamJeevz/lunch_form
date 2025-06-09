1525cabc0af12460890636e6cb026fe67ce1387d
IamJeevz <jeevansaju1@outlook.com>
2025-06-09 15:14:50 +0530

    time update

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify, render_template
 import requests
 from datetime import datetime
+from pytz import timezone
 
+india = timezone("Asia/Kolkata")
+now = datetime.now(india)
 app = Flask(__name__)
 
 # Google Form and Webhook URLs
